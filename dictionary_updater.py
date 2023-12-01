3ea53ca766b1f1fd85c599d863a2b3c37f31a42c
adi-prasetyo <theghostsquad00@gmail.com>
2023-12-01 15:43:09 +0900

    simplified dict updater
    although obsolete since now its updated every day

--- dictionary_updater.py
+++ dictionary_updater.py
@@ -2,7 +2,14 @@
 import pandas as pd
 
 from datetime import datetime
-from df_config import create_postgres_engine, create_aws_engine
+from sqlalchemy import create_engine
+
+def create_postgres_engine():
+    return create_engine("postgresql+psycopg2://postgres:lalala@localhost:5432/postgres")
+
+
+def create_aws_engine():
+    return create_engine('postgresql+psycopg2://postgres:taji3030postgres@tajimaya-products-instance-1.crjzkl0txyjn.ap-northeast-1.rds.amazonaws.com:5432/postgres')
 
 aws_engine = create_aws_engine()
 postgres_engine = create_postgres_engine()
@@ -10,19 +17,17 @@ postgres_engine = create_postgres_engine()
 df_ingredients_dictionary = pd.read_excel(r'D:\PycharmProjects\englishTranslate\ingredients\ingredients_dictionary.xlsx')
 df_ingredients_separator = pd.read_excel(r'D:\PycharmProjects\englishTranslate\ingredients\ingredients_separator.xlsx')
 
-df_ingredients_dictionary.to_sql('ingredients_dictionary', postgres_engine, if_exists='replace', index=False)
-df_ingredients_dictionary.to_sql('ingredients_dictionary', aws_engine, if_exists='replace', index=False)
-
-df_ingredients_separator.to_sql('ingredients_separator', postgres_engine, if_exists='replace', index=False)
-df_ingredients_separator.to_sql('ingredients_separator', aws_engine, if_exists='replace', index=False)
-
 # Update the dictionary_update_log table
 update_time = datetime.utcnow()
 df_log_update = pd.DataFrame({
     'dictionary_name': ['ingredients_dictionary', 'ingredients_separator'],
     'last_updated': [update_time, update_time]
 })
-df_log_update.to_sql('dictionary_update_log', postgres_engine, if_exists='replace', index=False)
-df_log_update.to_sql('dictionary_update_log', aws_engine, if_exists='replace', index=False)
 
+engines = [aws_engine, postgres_engine]
+
+for engine in engines:
+    df_ingredients_dictionary.to_sql('ingredients_dictionary', engine, if_exists='replace', index=False)
+    df_ingredients_separator.to_sql('ingredients_separator', engine, if_exists='replace', index=False)
+    df_log_update.to_sql('dictionary_update_log', engine, if_exists='replace', index=False)
 
